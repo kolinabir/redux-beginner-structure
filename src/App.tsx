1e3c59cdfa1eeb2075347f82dd83682e4e12a7f0
kolinabir <knkolin9@gmail.com>
2024-03-16 14:43:39 +0600

    Refactor code for improved performance

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div className="h-screen w-full flex justify-center items-center">
+      <div className="flex border border-purple-300 rounded-md p-10 bg-slate-200">
+        <button className="px-3 py-2 rounded-md bg-green-500 text-xl font-semibold text-white">
+          Increment
+        </button>
+        <h1 className="text-3xl mx-10">0</h1>
+        <button className="px-3 py-2 rounded-md bg-red-400 text-xl font-semibold text-white">
+          Decrement
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
